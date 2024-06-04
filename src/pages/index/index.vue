1ff154169d0ca7915b940e9b9ee3fabda930c2e9
HZJMaster <846163332@qq.com>
2024-06-04 14:05:50 +0800

    feat(all): use the uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <uni-card
+      title="基础卡片"
+      sub-title="副标题"
+      extra="额外信息"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+    </uni-card>
   </view>
 </template>
 
